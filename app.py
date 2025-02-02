0114892fb7a04822c25dc34e191dccebaf401a08
Deniz Mert Dilaverler <deniz.dilaverler@rwth-aachen.de>
2025-02-02 16:19:36 +0100

    [Task2 solution] Add copyPasteAnalysis

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
 import datetime
+from flask import Flask,jsonify
 app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
 from dataclasses import dataclass
 from sqlalchemy.orm import   MappedColumn, mapped_column
 from sqlalchemy import  BigInteger, ForeignKey 
+import pandas as pd
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///testdata.db'
 db = SQLAlchemy(app)
@@ -47,9 +48,30 @@ def index():
 
 @app.route('/copyPasteAnalysis')
 def copyPasteAnalysis():
-    # TODO
-    return {}
 
+    uilog_df = pd.read_sql_query("SELECT u.*, a.applicationname FROM uilog u JOIN applog a ON u.appid = a.id;", db.engine)
+    uilog_df["timestamp"] = pd.to_datetime(uilog_df["timestamp"])
+    uilog_cp = uilog_df[(uilog_df["eventtype"] == "CTRL + C") |
+                        (uilog_df["eventtype"] == "CTRL + X" )|
+                        ((uilog_df["eventtype"] == "Left-Down") &  (uilog_df["acceleratorkey"]=="STRG+C" ))
+    ]
+
+    uilog_pst = uilog_df[uilog_df["eventtype"] == "CTRL + V"]
+    
+    uilog_merged = pd.merge_asof(
+        left=uilog_pst.sort_values("timestamp"),
+        right=uilog_cp.sort_values("timestamp"),
+        by="userid",
+        direction="backward",
+        left_on="timestamp",
+        right_on="timestamp"
+    )
+
+    uilog_merged = uilog_merged.groupby(["applicationname_x", "applicationname_y"]).size().reset_index(name="count")
+    result = uilog_merged.rename(columns={"applicationname_x": "to", "applicationname_y": "from"})
+
+    # Return the data as a JSON response
+    return jsonify(result.to_dict("records"))
     
 if __name__ == "__main__":
     app.run(debug=True)
